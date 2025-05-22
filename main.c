e12cbd544e47de79e3d4801e576b8c1727a46afa
Youssef Essedraoui <yoessedr@e2r8p7.1337.ma>
2025-05-22 20:10:22 +0100

    main

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
 #include "minishell.h"
+
+int main(int ac, char **av, char **env)
+{
+    char *input;
+
+    t_node *envv = env_init(env);
+    while (1)
+    {
+        input = readline("myshell> ");
+        if (!input)
+            break;
+        // add_history(input); // Optional: adds input to history
+        if (input)
+        {
+            // function excute command
+        }
+        free(input); // must free the string returned
+    }
+
+}
